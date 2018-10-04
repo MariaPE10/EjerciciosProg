388443ed75c4f50b9a5c5cff10a6b62646b8f7c0
María Pérez <Mara Prez@MARIA-PC>
2018-10-04 19:58:28 +0200

    Bloque 2 Ejercicios Bucles

--- Ejercicios/src/Codigo/codigo02.java
+++ Ejercicios/src/Codigo/codigo02.java
@@ -43,23 +43,29 @@ public class codigo02 {
 		JOptionPane.showMessageDialog(null, "La suma de los numeros mayores de 10 es: " + suma);
 	}
 	
+	/* Ejercicio 3: Realiza un ejercicio que pida números al usuario, tantos como indique el usuario. al final
+	 * debe aparecer cuantos números positivos y negativos se han introducido.
+	 */
 	
 	public static void BuclesB1Ej3 () {
 		int n1, i, cantidadNumeros;
-		double suma=0d;
+		double contPos=0d;
+		double contNeg=0d;
 		
 		cantidadNumeros =Integer.parseInt(JOptionPane.showInputDialog("Introduzca la cantidad de numeros"));
 		
 		for (i = 0; i < cantidadNumeros; i++) {
 			n1 =Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero " + (i+1)));
-			if (n1 > 10) {
-				suma = suma + n1;
+			if (n1 >= 0) {
+				contPos++;
+			} else {
+				contNeg++;
 			}
 		}
-		JOptionPane.showMessageDialog(null, "La suma de los numeros mayores de 10 es: " + suma);
+		JOptionPane.showMessageDialog(null, "Cantidad de numeros positivos introducidos: " + contPos 
+				+ "\nCantidad de numeros negativos introducidos: " + contNeg);
 	}
-	
-	
+		
 	/* BLOQUE 02: Ejercicio1 1: Media de una cantidad de numeros introducidos por el usuario.
 	 * El usuario especificara una cantidad de numeros, tras lo cual se le deben pedir 
 	 * tantos como este haya indicado. Al final se debe imprimir la media. 
@@ -83,7 +89,7 @@ public class codigo02 {
 	/* Hacer lo mismo que en el anterior pero con un while. Se debe repetir hasta que el numero
 	 * introducido sea 0.
 	 */
-	public static void BuclesEj1B () {
+	public static void BuclesB2Ej1B () {
 		int num = 0;
 		int suma =0;
 		int contador =0;
@@ -110,15 +116,38 @@ public class codigo02 {
 	 */
 	
 	public static void BuclesB2Ej2 () {
-		int n1, i, cantidadNumeros, menor, mayor;
+		int n1 =0, i, cantidadNumeros, menor, mayor;
 		
 		cantidadNumeros =Integer.parseInt(JOptionPane.showInputDialog("Introduzca la cantidad de numeros"));
+		n1 =Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero 1"));
+		mayor = n1;
+		menor = n1;
 		
-		for (i = 0; i < cantidadNumeros; i++) {
-			n1 =Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero " + (i+1)));
+		for (i = 0; i < (cantidadNumeros-1); i++) { //cantidad de numeros -1 xq ya ha pedido un numero antes del bucle.
+			n1 =Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero " + (i+2)));
+			mayor = (n1 > mayor)? n1 : mayor;
+			menor = (n1 < menor)? n1 : menor;
+		}
+		JOptionPane.showMessageDialog(null,"El numero MAYOR es: " + mayor + "\n El numero MENOR es: " + menor);
+	}
+
+	/*Ejercicio 3: Ejercicio que obtenga todos los múltiplos de un número introducido por el usuario.
+	 * Se imprimirán todos los múltiplos menores de 100, este será el límite.
+	 */
+	public static void BuclesB2Ej3 () {
+		int num, contador = 1;
+		int resultado;
+			
+		num = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero"));
+		resultado = num;
+		System.out.println(num + " x " + contador + " = " + resultado);
+		
+		while (resultado < 100) { //se repetira mientras que lo que hay dentro del parentesis sea verdadero.
+			contador++;
+			resultado = num*contador;
+			System.out.println(num + " x " + contador + " = " + resultado);
 			}
 		}
-		JOptionPane.showMessageDialog(null,"El numero MAYOR es: " + maro + "\n El numero MENOR es: " + menor);
 
 }
 
--- Ejercicios/src/Ejercicios/Bucles.java
+++ Ejercicios/src/Ejercicios/Bucles.java
@@ -5,7 +5,7 @@ import Codigo.codigo02;
 public class Bucles {
 
 	public static void main(String[] args) {
-		codigo02.BuclesB1Ej2();
+		codigo02.BuclesB2Ej3();
 
 	}
 
