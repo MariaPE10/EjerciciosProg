a29eb68d9ab244e2e7704e7a5d4e11f57d22a63e
diurno <diurno@pc-aula>
2018-10-08 11:30:19 +0200

    Ejercicios con WHILE y DOWHILE

--- Ejercicios/src/Codigo/codigo02.java
+++ Ejercicios/src/Codigo/codigo02.java
@@ -56,7 +56,7 @@ public class codigo02 {
 		for (int i = 0; i < cantidadNumeros; i++) {
 			n1 =Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero " + (i+1)));
 			if (n1 >= 0) {
-				contPos++;
+				contPos++; //operador incremento
 			} else {
 				contNeg++;
 			}
@@ -167,5 +167,123 @@ public class codigo02 {
 		}
 	}
 
+	/*BLOQUE 3 HECHO CON WHILE
+	* Ejercicio 1: Realiza un ejercicio que pida números al usuario. El programa debe detenerse cuando el usuario introduzca
+	* el número 0 (cero), que no debe tenerse en cuenta para ninguna operación aritmética, simplemente para salir
+	* de la aplicación. Cuando el programa haya terminado, se debe sacar en pantalla el valor de la suma de todos
+	* los números. */
+	
+	public static void WhileB3Ej1 () {
+		int num=1, suma=0;
+		String mensaje = "Introduzca n�mero (0 -> Terminar)";
+		
+		while (num != 0) {
+			num =Integer.parseInt(JOptionPane.showInputDialog(mensaje));
+			suma= suma +num;
+		}
+		JOptionPane.showMessageDialog(null,"La suma de los numeros es: " + suma);
+	}
+	
+	/* Ejercicio 2: Realiza un ejercicio igual al anterior, con la particularidad de imprimir la 
+	 * media al final del ejercicio, no la suma.
+	 */
+	
+	public static void WhileB3Ej2 () {
+		int num=1, suma=0, contador=0;
+		double media=0d;
+		String mensaje = "Introduzca n�mero (0 -> Terminar)";
+		
+		while (num != 0) {
+			num =Integer.parseInt(JOptionPane.showInputDialog(mensaje));
+			suma= suma +num;
+			contador++;
+		}
+		media = (double)suma/(contador-1);
+		JOptionPane.showMessageDialog(null,"La media de los numeros es: " + media);
+	}
+	
+	/* Ejercicio 3:  Realiza un ejercicio que pida números al usuario hasta que este introduzca el 0 (cero).
+	 * Al finalizar el ejercicio se debe imprimir en pantalla el valor mayor introducido. El valor 0 (cero) no
+	 * debe tenerse en cuenta.
+	 */
+
+	public static void WhileB3Ej3 () {
+		int num=1, mayor=0;
+		String mensaje = "Introduzca n�mero (0 -> Terminar)";
+		
+		num =Integer.parseInt(JOptionPane.showInputDialog(mensaje));
+		mayor = num;
+		
+		while (num != 0) {
+			if (num > mayor) {
+				mayor = num;
+			}
+		num =Integer.parseInt(JOptionPane.showInputDialog(mensaje));
+		}
+		JOptionPane.showMessageDialog(null,"El numero mayor es: " + mayor);
+	}
+	
+	/* BLOQUE EJERCICIOS 3 HEHCO CON DO WHILE
+	 * Ejercicio 1: Realiza un ejercicio que pida números al usuario. El programa debe detenerse cuando el usuario introduzca
+	 * el número 0 (cero), que no debe tenerse en cuenta para ninguna operación aritmética, simplemente para salir
+	 * de la aplicación. Cuando el programa haya terminado, se debe sacar en pantalla el valor de la suma de todos
+	 * los números. */
+	
+	public static void DoWhileB3Ej1 () {
+		int num, suma=0;
+		String mensaje = "Introduzca n�mero (0 -> Terminar)";
+		
+		do { 
+			num =Integer.parseInt(JOptionPane.showInputDialog(mensaje));
+			suma= suma +num;
+		} while (num != 0);
+		JOptionPane.showMessageDialog(null,"La suma de los numeros es: " + suma);
+	}
+	
+	/* Ejercicio 2: Realiza un ejercicio igual al anterior, con la particularidad de imprimir la 
+	 * media al final del ejercicio, no la suma.
+	 */
+	
+	public static void DoWhileB3Ej2 () {
+		int num=1, suma=0, contador=0;
+		double media=0d;
+		String mensaje = "Introduzca n�mero (0 -> Terminar)";
+		
+		do {
+			num =Integer.parseInt(JOptionPane.showInputDialog(mensaje));
+			suma= suma +num;
+			contador++;
+		} while (num != 0);
+		media = (double)suma/(contador-1);
+		JOptionPane.showMessageDialog(null,"La media de los numeros es: " + media);
+	}
+	
+	/* Ejercicio 3:  Realiza un ejercicio que pida números al usuario hasta que este introduzca el 0 (cero).
+	 * Al finalizar el ejercicio se debe imprimir en pantalla el valor mayor introducido. El valor 0 (cero) no
+	 * debe tenerse en cuenta.
+	 */
+
+	public static void DoWhileB3Ej3 () {
+		int num=1, mayor=0;
+		String mensaje = "Introduzca n�mero (0 -> Terminar)";
+		
+		num =Integer.parseInt(JOptionPane.showInputDialog(mensaje));
+		mayor = num;
+		
+		do {
+			if (num > mayor) {
+				mayor = num;
+			}
+		num =Integer.parseInt(JOptionPane.showInputDialog(mensaje));
+		} while (num != 0);
+		JOptionPane.showMessageDialog(null,"El numero mayor es: " + mayor);
+	}
+	
+	
+	
+	
+	
+	
+	
 }
 
