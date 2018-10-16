2e13a573dbc1ee2c73c6d0eb89fe00f49d24ae19
María Pérez <Mara Prez@MARIA-PC>
2018-10-16 21:42:02 +0200

    asd kk de arrays

--- Ejercicios/src/Codigo/codigo03.java
+++ Ejercicios/src/Codigo/codigo03.java
@@ -206,22 +206,36 @@ public class codigo03 {
 		
 		num=Integer.parseInt(JOptionPane.showInputDialog("Introduzca el multiplicador"));
 		
-		System.out.println("ARRAY NUMERO 1\n");
+		System.out.println("ARRAY\n");
 		for (int i = 0; i<array.length; i++) {
 			array[i]= (int) Math.round(Math.random()*100);
 			System.out.print("["+i+"]" + " ");
 			System.out.print(array[i] + " ");
 		}
-		
+		System.out.println("\nARRAY MULTIPLICADO POR: " + num);
 		for (int i = 0; i<array.length; i++) {
-			array[i]= (int) Math.round(Math.random()*100);
-
+			array[i]= array[i]*num;
+			System.out.print("["+i+"]" + " ");
+			System.out.print(array[i] + " ");
 		}
 	}
 	
+	/* Ejercicio 4: Realiza un ejercicio en que inicialice un array de 5 elementos enteros al azar entre 0 y 100.
+	 * Debes conseguir que se desplacen los números un lugar a su derecha, de manera que el desplazamiento sea circular,
+	 * si un número sale por la derecha volverá a entrar por la izquierda. Ejemplo: 1 2 3 4 5 pasa a ser 5 1 2 3 4 que
+	 * pasa a ser 4 5 1 2 3.
+	 */
 	
-	
-	
+	public static void ArrayB2Ej4() {
+		int array[]= new int [5];
+		
+		System.out.println("ARRAY\n");
+		for (int i = 0; i<array.length; i++) {
+			array[i]= (int) Math.round(Math.random()*100);
+			System.out.print(array[i] + " ");
+		}
+		
+	}
 	
 	
 	
