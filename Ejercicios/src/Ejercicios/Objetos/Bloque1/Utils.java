ae5df41b3d2baab354a59d17dc32da7442889203
María Pérez <Mara Prez@MARIA-PC>
2018-10-23 23:40:23 +0200

    asdaadf

---
+++ Ejercicios/src/Ejercicios/Objetos/Bloque1/Utils.java
@@ -0,0 +1,62 @@
+package Ejercicios.Objetos.Bloque1;
+
+import javax.swing.JOptionPane;
+
+public class Utils {
+
+/* Ejercicio 1: Realiza un método que reciba como parámetros de entrada tres valores enteros y uno flotante.
+  * El método debe devolver la media aritmética de los cuatro valores, un valor flotante.
+  * "main" debe usar a esta función creada por ti, e imprimir el valor de la media en la consola de salida.
+  */
+	
+	public int sumaEnteros(int num1, int num2, int num3) {
+		
+		int acumulado = 0;
+		
+		acumulado += num1;
+		acumulado += num2;
+		acumulado += num3;
+		
+		return acumulado;
+	}
+	
+	public float sumaTotal(float numeroFloat, int numeroEntero1, int numeroEntero2, int numeroEntero3) {		
+		
+		float sumaTotal = sumaEnteros(numeroEntero1,numeroEntero2,numeroEntero3) + numeroFloat;
+		
+		return sumaTotal;		
+	}
+
+	public float mediaEnteros(int numeroEntero1, int numeroEntero2, int numeroEntero3) {
+		
+		float mediaEnteros = 0;
+		
+		mediaEnteros = sumaEnteros(numeroEntero1,numeroEntero2,numeroEntero3)/3;
+		
+		return mediaEnteros;
+	}
+	
+	public float mediaTotal(float numeroFloat, int numeroEntero1, int numeroEntero2, int numeroEntero3) {
+		
+		float mediaTotal = 0;
+		
+		mediaTotal = sumaTotal(numeroFloat, numeroEntero1, numeroEntero2, numeroEntero3)/4;
+		
+		return mediaTotal;
+	}
+	
+	public static void main(String[] args) {
+		int numeroPedido1 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el primer numero"));
+		int numeroPedido2 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el segundo numero"));
+		int numeroPedido3 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el tercer numero"));
+		float numeroPedido4 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca el cuarto numero"));
+		
+		Utils objetoMedia = new Utils();
+		objetoMedia.sumaEnteros();
+		
+		System.out.println("Suma total: " + objetoMedia.sumaTotal());
+		System.out.println("Media total: " + objetoMedia.mediaTotal());
+	}
+	
+}
+
