68c3737091c807d769089ec00aacde79633d57e7
diurno <diurno@pc-aula>
2018-11-26 14:58:21 +0100

    ahorcado

---
+++ Ejercicios/src/Ejercicios/Objetos/Ahorcado/Palabra.java
@@ -0,0 +1,43 @@
+package Ejercicios.Objetos.Ahorcado;
+
+public class Palabra {
+
+	
+//	
+//	public static void ComparaCaracteres(String caracterUsuario, String palabraAleatoria, String coincidencias[]){
+//		if (CompruebaPalabraOCaracter(caracterUsuario) == true){
+//			for (int i = 0; i < coincidencias.length; i++) {
+//				if (palabraAleatoria.charAt(i) (caracterUsuario)) {
+//					coincidencias[i].replace(palabraAleatoria.charAt(i), caracterUsuario);
+//				} else {
+//					coincidencias[i] = "_ ";
+//				}
+//			}
+//		}
+//		ImprimePalabraOculta(coincidencias);
+//	}	
+//	
+	/**
+	 * Devuelve true cuando el usuario introduce un caracter y false si introduce una palabra
+	 * @param palabraUsuario
+	 * @param palabraAleatoria
+	 * @return
+	 */
+	public static boolean CompruebaPalabraOCaracter(String palabraUsuario) {
+		if (palabraUsuario.length() == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	public static void ImprimePalabraOculta(String coincidencias[]) {
+		for (int i = 0; i < coincidencias.length; i++) {
+			System.out.print("_ ");
+		}
+	}
+
+}
---
+++ Ejercicios/src/Ejercicios/Objetos/Ahorcado/Principal.java
@@ -0,0 +1,30 @@
+package Ejercicios.Objetos.Ahorcado;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		//Array de palabras posibles
+		String palabras[] = new String[] {"Sepultura", "Jaula", "Olas", "Articulacion", "Negativo", "Inflacion", "Barro"};
+		
+		//Genera un numero aleatorio que será mi palabra aleatoria
+//		int palabraAleatoria = (int) Math.round(Math.random()*(7));
+		int indicePalabraAleatoria = 2;
+		
+		//Creacion de un array con la longitud de nuestra palabra aleatoria
+		String palabraAleatoria = palabras[indicePalabraAleatoria];
+		String coincidencias[]= new String [palabraAleatoria.length()];
+		
+		
+		//Imprime la palabra aleatoria oculta 
+		Palabra.ImprimePalabraOculta(coincidencias);
+		
+		//Pide al usuario la palabra
+		String palabraUsuario = String.valueOf(JOptionPane.showInputDialog(null, "Introduzca el caracter o la palabra buscada"));
+		Palabra.ComparaCaracteres(palabraUsuario, palabraAleatoria, coincidencias);
+		
+	}
+
+}
